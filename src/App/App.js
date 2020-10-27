881486d426640d5c103fd9b5129e0383e97c6c35
liyixuan <13069579956@163.com>
2020-10-27 21:50:27 +0800

    add List

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import List from './List'
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        H
+        <List/>
       </div>
     );
   }
